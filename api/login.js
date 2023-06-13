5c7b2297ef3d79a40fa761be8e8045e8f746e26d
dangchien2003 <chienboy03@gmail.com>
2023-06-14 00:29:05 +0700

    Revert "Update login.js"
    This reverts commit 5454032cd52d1a0cec63ec52dfa5828477c2f8df.

--- api/login.js
+++ api/login.js
@@ -3,7 +3,10 @@ const router = express.Router();
 
 const {checkRequest, checkExist} = require('../middleware/login/login');
 
-
+//get http://127.0.0.1:3000/login/
+router.get('/', (req, res)=> {
+    res.send("trang login")
+})
 
 //post http://127.0.0.1:3000/login/
 router.post('/',checkRequest, checkExist)
